4bc1965eaec0a032e059c384802810d3569eb685
sona2308 <sonalp232323@gmail.com>
2023-04-25 16:33:40 +0530

    Write a JavaScript code. Given n as input, print on the console Good if n>=75, Average, if n is greater than equal to 50 but less than 75 and Poor otherwise. Take n=63 and output expected is Average

---
+++ task10.js
@@ -0,0 +1,14 @@
+/* 
+Write a JavaScript code.
+Given n as input, print on the console Good if n>=75, Average,
+if n is greater than equal to 50 but less than 75 and Poor otherwise. 
+Take n=63 and output expected is Average 
+*/
+const n = 63;
+if (n >= 75) {
+  console.log('Good');
+} else if (n >= 50) {
+  console.log('Average');
+} else {
+  console.log('Poor');
+}
