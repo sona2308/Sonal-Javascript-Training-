3dc0ce5d9cb20c1314682fb023fff934c56665f8
sona2308 <sonalp232323@gmail.com>
2023-04-25 12:03:36 +0530

    question

--- file1.js
+++ file1.js
@@ -1,5 +1,12 @@
+/*
+Write a JavaScript code  
+Given a number n. 
+If n is greater than 0, 
+display the message Positive Number otherwise 
+display Not a Positive Number in the console ,
+Take Input is n=7 and output expected is Not a Positive Number
+*/
 let n = 7;
-
 if (n > 0) {
   console.log("Not a Positive Number");
 } else {
