2fc9c5b32bb1fb381e352870de38243ff29d36a6
sona2308 <sonalp232323@gmail.com>
2023-04-25 12:13:37 +0530

    Write a JavaScript code. Given a number n. If n is a multiple of 10, display the message Multiple of 10, otherwise display Not a Multiple of 10in the console. Take  n=99 and output expected is Not a Multiple of 10

---
+++ task3.js
@@ -0,0 +1,13 @@
+/*
+Write a JavaScript code.
+Given a number n.If n is a multiple of 10,
+display the message Multiple of 10, 
+otherwise display Not a Multiple of 10in the console. 
+Take n = 99 and output expected is Not a Multiple of 10 
+*/
+let n = 99;
+if (n % 10 === 0) {
+  console.log("Multiple of 10");
+} else {
+  console.log("Not a Multiple of 10");
+}
