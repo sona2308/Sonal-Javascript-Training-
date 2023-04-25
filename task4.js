0e5597d77db1cfbfb7ecc17ab6f46d194df31b50
sona2308 <sonalp232323@gmail.com>
2023-04-25 12:29:07 +0530

    Given 2 numbers m and n. If m is greater than n, display Larger in the console. If they are equal display Equal. If m is less than n, display Smaller.Take  m=10, n=3 and output expected is Larger.

---
+++ task4.js
@@ -0,0 +1,16 @@
+/*
+Write a JavaScript code. Given 2 numbers m and n. 
+If m is greater than n, display Larger in the console. 
+If they are equal display Equal. 
+If m is less than n, display Smaller. 
+Take  m=10, n=3 and output expected is Larger. 
+*/
+let m = 10;
+let n = 3;
+if (m > n) {
+  console.log("Larger");
+} else if (m < n) {
+  console.log("Smaller");
+} else {
+  console.log("Equal");
+}
