321a3c26dec7485cb731eeb686693e0dc35b3ad5
sona2308 <sonalp232323@gmail.com>
2023-04-27 17:44:55 +0530

    // Given a String str, print Long on the console if its length is greater than 5 otherwise print Short // Tale input str = Edufect and out put expected is Long

---
+++ String/stringtask2.js
@@ -0,0 +1,8 @@
+// Given a String str, print Long on the console if its length is greater than 5 otherwise print Short 
+// Tale input str = Edufect and out put expected is Long
+const str = "Edufect";
+if (str.length > 5) {
+  console.log("Long");
+} else {
+  console.log("Short");
+}
