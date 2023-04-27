7729d2520185e28ef336c7cd4e2edaf8847f0c30
sona2308 <sonalp232323@gmail.com>
2023-04-27 18:29:22 +0530

    Stringtask7

---
+++ String/stringtask7.js
@@ -0,0 +1,9 @@
+/*
+Write a very simple JavaScript code. Write a biginner level javascript code. Given a String str, create a new String in which zhas been replaced by x. Display the new string in the console. Display the new string in the console. Take input str=sleep=zzz and out put expected is sleep=xxx
+
+*/
+
+const str = "sleep=zzz";
+const newStr = str.replace(/z/g, "x");
+console.log(newStr);
+
