4b2e48b7b137fcf0cca6d3fc59ac0fea59494a59
sona2308 <sonalp232323@gmail.com>
2023-04-27 18:28:44 +0530

    Stringtask5

--- String/stringtask5.js
+++ String/stringtask5.js
@@ -1,17 +1,17 @@
-/* 
-Given a String str, 
-print the indexes at which a or b occur . 
+/*
+Write a very simple JavaScript code without using $ symbol. 
+Given a String str, print the indexes at which a or b occur . 
 Take input str=abca and output expected is  
 a at Index:0 
 b at Index:1 
-a at Index:3 
-*/
+a at Index:3
 
+*/
 const str = "abca";
-for(let i = 0; i < str.length; i++) {
-  if(str[i] === 'a' || str[i] === 'A') {
-    console.log(`a at Index:${i}`);
-  } else if(str[i] === 'b' || str[i] === 'B') {
-    console.log(`b at Index:${i}`);
+for (let i = 0; i < str.length; i++) {
+  if (str.charAt(i) === "a" || str.charAt(i) === "A") {
+    console.log("a at Index:" + i);
+  } else if (str.charAt(i) === "b" || str.charAt(i) === "B") {
+    console.log("b at Index:" + i);
   }
 }
