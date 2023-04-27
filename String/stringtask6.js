5dc7d13470a0571019289308ce6d69414f7dc7e4
sona2308 <sonalp232323@gmail.com>
2023-04-27 18:29:05 +0530

    Stringtask6

---
+++ String/stringtask6.js
@@ -0,0 +1,10 @@
+/*
+Write a JavaScript code. Given a String str, create a new String in which the first character has been removed. 
+Display the new string in the console. 
+Take input str=123456789 and 
+out put expected is 23456789 
+*/
+
+const str = "123456789";
+const newStr = str.slice(1);
+console.log(newStr);
